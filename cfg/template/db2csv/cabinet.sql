70e33b352cf4d6848add9832bf14cf1449c21837
zhangsutang <zhangsutang@163.com>
2016-03-12 17:52:25 +0800

    Revert "Test branch"

--- cfg/template/db2csv/cabinet.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-	`cab_id`, `cab_name`, `cab_iid`, `cab_path`
- INTO OUTFILE '{CSVDIR}' 
- CHARACTER SET {CHSET} 
- FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' 
- LINES TERMINATED BY '\n' 
- FROM wlddb.cabinet;
--- cfg/template/db2csv/report.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-	`type`, station, `no`, `name`, hex(attribute)
- INTO OUTFILE '{CSVDIR}' 
- CHARACTER SET {CHSET} 
- FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' 
- LINES TERMINATED BY '\n' 
- FROM wlddb.reportcfg;
